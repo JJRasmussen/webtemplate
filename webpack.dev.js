d7de1776f86a36f0b22eb065e30a9a821015ab7a
Jeppe <161380852+JJRasmussen@users.noreply.github.com>
2024-11-27 14:26:20 +0100

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: "development",
-    devtools: "inline-source-map",
+    devtool: "inline-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
     },
